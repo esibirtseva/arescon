b507249585711a26af75c047b874cc0f80f85197
Maxim Yakovlev <mail4mayase@gmail.com>
2014-12-04 00:49:45 +0300

    clickable devices

--- dist/scripts/chart.js
+++ dist/scripts/chart.js
@@ -1,6 +1,8 @@
 var data = [
     {
+        htmlId: "d1",
         name: "Счетчик Techem AP",
+        image: "images/water.jpg",
         description: "Водосчетчик Techem серий АР для горячей и холодной воды",
         dataDailyUsage : {
             labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
@@ -34,7 +36,9 @@ var data = [
 
     },
     {
+        htmlId: "d3",
         name: "Счетчик ГРАНД-25Т",
+        image: "images/gas.jpg",
         description: "Электронные бытовые счетчики газа ГРАНД-25Т предназначены для измерения объема газа, расходуемого газопотребляющим оборудованием с суммарным максимальным расходом до 25 м3/час",
         dataDailyUsage : {
             labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
@@ -68,7 +72,9 @@ var data = [
 
     },
     {
+        htmlId: "d2",
         name: "Счетчик однофазный СОЭ-52",
+        image: "images/electro.jpg",
         description: "Электросчётчики СОЭ-52 предназначены для учёта потребления электроэнергии в двухпроводных цепях электрического тока в закрытых помещениях",
         dataDailyUsage : {
             labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
@@ -102,8 +108,10 @@ var data = [
 
     },
     {
+        htmlId: "d4",
         name: "Счетчик СВ-15 Х \"МЕТЕР\"",
         description: "Счетчики воды крыльчатые СВ-15Х (одноструйные, сухоходные) предназначены для измерения объема горячей воды, протекающей по трубопроводу при температуре от 5°С до 90°С и рабочем давлении в водопроводной сети не более 1, 0 МПа",
+        image: "images/water-2.jpeg",
         dataDailyUsage : {
             labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
             datasets: [
@@ -136,10 +144,15 @@ var data = [
 
     },
 ]
-var ctxDailyUsage, dailyUsageChart, dataDailyUsage, optionsDailyUsage;
+var ctxDailyUsage, dailyUsageChart, dataDailyUsage, optionsDailyUsage, dailyDataSetsStore;
 window.onload = function(){
+    //devices
+    for (var i = 0; i < data.length; i++){
+        var currentDevice = $('#'+data[i].htmlId);
+        currentDevice.children('.device_info').children('h4').html(data[i].name);
+    }
     //daterange
-    var a = $('input[name="daterange"]').daterangepicker(
+    $('input[name="daterange"]').daterangepicker(
         {
             ranges: {
                 'Сегодня': [moment(), moment()],
@@ -167,7 +180,6 @@ window.onload = function(){
         }
     );
 
-    console.log(a);
     //daily
     ctxDailyUsage = $("#dailyUsageChart").get(0).getContext("2d");
     ctxDailyUsage.canvas.width = $("#dailyUsageChart").parent().width();
@@ -175,7 +187,7 @@ window.onload = function(){
         labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
         datasets: [
             {
-                label: "My Second dataset",
+                label: "Холодная вода",
                 fillColor: "rgba(151,187,205,0.2)",
                 strokeColor: "rgba(151,187,205,1)",
                 pointColor: "rgba(151,187,205,1)",
@@ -183,14 +195,29 @@ window.onload = function(){
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(151,187,205,1)",
                 data: [28, 48, 40, 19, 86, 27, 90]
+            },
+            {
+                label: "Горячая вода",
+                fillColor: "rgba(231, 75, 59, 0.2)",
+                strokeColor: "#E74B3B",
+                pointColor: "#E74B3B",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [38, 28, 30, 49, 56, 47, 60]
             }
         ]
     };
     optionsDailyUsage = {
         scaleShowGridLines : false,
+        showTooltips: true,
+        responsive: true,
+        legendTemplate : "<div class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=-1; i<datasets.length; i++){%><%if(!datasets[i]){%><p onclick=\"focusDataSet(<%=i%>)\"><span>●</span>Показать всё</p><%} else {%><p onclick=\"focusDataSet(<%=i%>)\"><span style=\"color:<%=datasets[i].strokeColor%>\">●</span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></p><%}}%></div>"
     };
     dailyUsageChart = new Chart(ctxDailyUsage).Line(dataDailyUsage, optionsDailyUsage);
+    $('#dailyUsageLegend').html(dailyUsageChart.generateLegend());
 
+    dailyDataSetsStore = jQuery.extend({},dailyUsageChart.datasets);
     //share
     var ctxShareUsage = $("#shareUsageChart").get(0).getContext("2d");
     ctxShareUsage.canvas.width = $("#shareUsageChart").parent().width();
@@ -209,24 +236,6 @@ window.onload = function(){
         }
     ];
     var optionsShareUsage = {
-	// Boolean - Whether to show labels on the scale
-    scaleShowLabels: true,
-
-    // Interpolated JS string - can access value
-    scaleLabel: "<%=value%>",
-
-    // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
-    scaleIntegersOnly: true,
-
-    // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-    scaleBeginAtZero: false,
-
-    // String - Scale label font declaration for the scale label
-    scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-    // Number - Scale label font size in pixels
-    scaleFontSize: 12,
-	
         //Boolean - Whether we should show a stroke on each segment
         segmentShowStroke : true,
 
@@ -259,6 +268,40 @@ window.onload = function(){
 };
 $(window).resize(function(){
     if (!ctxDailyUsage) return;
-    ctxDailyUsage.canvas.width = $('.data').width();
-    dailyUsageChart = new Chart(ctxDailyUsage).Line(dataDailyUsage, optionsDailyUsage);
+    // ctxDailyUsage.canvas.width = $('.data').width();
+    // dailyUsageChart = new Chart(ctxDailyUsage).Line(dataDailyUsage, optionsDailyUsage);
+    
+    dailyUsageChart.resize();
+    // dailyUsageChart.update();
 });
+var focusDataSet = function(index){
+    dailyUsageChart.datasets = jQuery.extend({},dailyDataSetsStore);
+    if (index == -1){
+        dailyUsageChart.update();
+        return;  
+    } 
+    for (var i = 0; dailyUsageChart.datasets[i]; i++){
+        if (i != index) dailyUsageChart.datasets[i] = {};
+    } 
+    dailyUsageChart.update();
+}
+$('.device').click(function(){
+    $('.device').removeClass('active');
+    var currentDevice = $(this);
+    currentDevice.addClass('active');
+    var id = currentDevice.attr('id');
+    showDevice(id);
+});
+var showDevice = function(htmlId){
+    var index = -1;
+    for (var i = 0; i < data.length; i++){
+        if (data[i].htmlId == htmlId) index = i;
+    }
+    console.log(index);
+    if (index != -1){
+        var currentDeviceData = data[index];
+        $('.data .device_name h4').html(currentDeviceData.name);
+        $('.data .device_name p').html(currentDeviceData.description);
+        $('.data .device_image').css("background-image", "url(" + currentDeviceData.image + ")");
+    }
+}
