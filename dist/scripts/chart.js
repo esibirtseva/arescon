3e1815b66254fd444702872381e9d4a405c609ef
Maxim Yakovlev <mail4mayase@gmail.com>
2014-12-03 03:44:50 +0300

    improved map

--- dist/scripts/chart.js
+++ dist/scripts/chart.js
@@ -1,5 +1,144 @@
+var data = [
+    {
+        name: "Счетчик Techem AP",
+        description: "Водосчетчик Techem серий АР для горячей и холодной воды",
+        dataDailyUsage : {
+            labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
+            datasets: [
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        },
+        dataShareUsage : [
+            {
+                value: 350,
+                color:"rgba(66, 139, 202, 0.1)",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 100,
+                color: "rgb(66, 139, 202)",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ]
+
+    },
+    {
+        name: "Счетчик ГРАНД-25Т",
+        description: "Электронные бытовые счетчики газа ГРАНД-25Т предназначены для измерения объема газа, расходуемого газопотребляющим оборудованием с суммарным максимальным расходом до 25 м3/час",
+        dataDailyUsage : {
+            labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
+            datasets: [
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        },
+        dataShareUsage : [
+            {
+                value: 350,
+                color:"rgba(66, 139, 202, 0.1)",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 100,
+                color: "rgb(66, 139, 202)",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ]
+
+    },
+    {
+        name: "Счетчик однофазный СОЭ-52",
+        description: "Электросчётчики СОЭ-52 предназначены для учёта потребления электроэнергии в двухпроводных цепях электрического тока в закрытых помещениях",
+        dataDailyUsage : {
+            labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
+            datasets: [
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        },
+        dataShareUsage : [
+            {
+                value: 350,
+                color:"rgba(66, 139, 202, 0.1)",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 100,
+                color: "rgb(66, 139, 202)",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ]
+
+    },
+    {
+        name: "Счетчик СВ-15 Х \"МЕТЕР\"",
+        description: "Счетчики воды крыльчатые СВ-15Х (одноструйные, сухоходные) предназначены для измерения объема горячей воды, протекающей по трубопроводу при температуре от 5°С до 90°С и рабочем давлении в водопроводной сети не более 1, 0 МПа",
+        dataDailyUsage : {
+            labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"],
+            datasets: [
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        },
+        dataShareUsage : [
+            {
+                value: 350,
+                color:"rgba(66, 139, 202, 0.1)",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 100,
+                color: "rgb(66, 139, 202)",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ]
+
+    },
+]
 var ctxDailyUsage, dailyUsageChart, dataDailyUsage, optionsDailyUsage;
 window.onload = function(){
+    console.log(data);
     //daily
     ctxDailyUsage = $("#dailyUsageChart").get(0).getContext("2d");
     ctxDailyUsage.canvas.width = $("#dailyUsageChart").parent().width();
--- dist/scripts/map.js
+++ dist/scripts/map.js
@@ -18,10 +18,12 @@ window.onload = function(){
 		myMap.controls.remove('fullscreenControl');
 		myMap.controls.remove('typeSelector');
 
-		for (var i = 0; i < points.length - 1; i++){
-			addHouse(points[i][0], points[i][1], false);
+		for (var i = 0; i < points.length; i++){
+			var placemark = addHouse(points[i].coords[0], points[i].coords[1], false);
+			points[i].placemark = placemark;
 		}
-		addHouse(points[3][0], points[3][1], true);
+		// var placemark = addHouse(points[3].coords[0], points[3].coords[1], true);
+		// points[3].placemark = placemark;
 
 		//events
 		// myMap.events.add('click', function (e) {
@@ -38,13 +40,88 @@ window.onload = function(){
 			preset: isProblem?red:blue
 		});
 		myMap.geoObjects.add(myPlacemark);
+		return myPlacemark;
 	}
+
+
 	var points = [
-		[55.71855041425817, 37.66815246582029],
-		[55.78283647321973, 37.55691589355467],
-		[55.706921098504964, 37.470398559570306],
-		[55.87404807445789, 37.690125122070306],
+		{
+			id: 1,
+			coords: [55.71855041425817, 37.66815246582029]
+		},
+		{
+			id: 2,
+			coords: [55.78283647321973, 37.55691589355467]
+		},
+		{
+			id: 3,
+			coords: [55.706921098504964, 37.470398559570306]
+		},
+		{
+			id: 4,
+			coords: [55.87404807445789, 37.690125122070306]
+		}
 	];
+	var tszhs_data = [
+		{
+			id: 'tszh1',
+			houses: [1, 2]
+		},
+		{
+			id: 'tszh2',
+			houses: [3, 4]
+		}
+	];
+	var houses_data = [
+		{
+			id: 'house1',
+			point: 1
+		},
+		{
+			id: 'house2',
+			point: 2
+		},
+		{
+			id: 'house3',
+			point: 3
+		},
+		{
+			id: 'house4',
+			point: 4
+		}
+	];
+	$('.tszhs .item p').click(function(){
+		points_to_default();
+		var id = $(this).parent().attr('id');
+		for (var i = 0; i < tszhs_data.length; i++){
+			if (id === tszhs_data[i].id){
+				highlight_points(tszhs_data[i].houses);
+			}
+		}
+	});
+	$('.houses .item p').click(function(){
+		points_to_default();
+		var id = $(this).parent().attr('id');
+		for (var i = 0; i < houses_data.length; i++){
+			if (id === houses_data[i].id){
+				highlight_points([houses_data[i].point]);
+				console.log("highlight " + houses_data[i].point);
+			}
+		}
+	});
+	var highlight_points = function(arr_of_ids){
+		for(var i = 0; i < points.length; i++){
+			if(arr_of_ids.indexOf(points[i].id) > -1){
+				points[i].placemark.options.set('preset', 'islands#redCircleDotIcon');
+			}
+		}
+	};
+	var points_to_default = function(){
+		for(var i = 0; i < points.length; i++){
+			points[i].placemark.options.set('preset', 'islands#blueCircleDotIcon');
+			
+		}
+	}
 }
 $(window).resize(function(){
 	var map_container = $('#map');
