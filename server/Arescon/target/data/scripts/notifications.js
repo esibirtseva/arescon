b25267fb41eecee80597fe7bb6419fce3dfb6f68
Striker <mironov1708@gmail.com>
2015-03-30 20:47:36 +0300

    '/notifications' almost done, v1

--- server/Arescon/target/data/scripts/notifications.js
+++ server/Arescon/target/data/scripts/notifications.js
@@ -12,19 +12,64 @@ $('.limit input').keyup(function () {
     this.value = this.value.replace(/[^0-9\.]/g,'');
 });
 
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+$('#save_notification').click(function(){
+    function getFormData(dom_query){
+        var out = {},
+            s_data = $(dom_query).serializeArray(),
+            tempArr;
+        //transform into simple data/value object
+        for(var i = 0; i<s_data.length; i++){
+            var record = s_data[i];
+            if(!out[record.name]) {
+                out[record.name] = record.value;
+            } else {
+                if (typeof out[record.name] == "string") {
+                    tempArr = (new Array(out[record.name]));
+                } else if (typeof out[record.name] == "array") {
+                    tempArr = out[record.name];
+                }
+                tempArr.push(record.value);
+                out[record.name] = tempArr;
+            }
+
+        }
+        return out;
+    }
+
+    var obj = getFormData('form');
+
+    // setting params from URI
+    obj['deviceId'] = getParameterByName('id');
+
+    console.log(obj); // obj - send to ajax request
+
+    $.post('/notifications/create', obj, function(data){
+        var currentData = JSON.parse(data);
+
+        // do some magic on response
+    });
+});
+
 var getTimeFormatddmmyyyy = function(date_ms){
     var current_date = new Date(date_ms);
-    var date_str = current_date.getDate() + "." + (current_date.getMonth()+1) + "." + current_date.getFullYear();
-    return date_str;
+    return current_date.getDate() + "." + (current_date.getMonth()+1) + "." + current_date.getFullYear();
 };
 
 var initDateRangePicker = function(){
-    var date = new Date();
-    currentEnd = date.getTime();
+    var date = new Date(),
+        currentEnd = date.getTime();
     // start on current month
-    currentStart = (new Date(date.getFullYear(), date.getMonth(), 1)).getTime();
-    $('input[name="daterange"]').val(getTimeFormatddmmyyyy(currentStart) + " - " + getTimeFormatddmmyyyy(currentEnd));
-    $('input[name="daterange"]').daterangepicker(
+    var currentStart = (new Date(date.getFullYear(), date.getMonth(), 1)).getTime();
+    var dateRange = $('input[name="daterange"]');
+    dateRange.val(getTimeFormatddmmyyyy(currentStart) + " - " + getTimeFormatddmmyyyy(currentEnd));
+    dateRange.daterangepicker(
         {
             format: 'DD.MM.YYYY',
             ranges: {
@@ -49,8 +94,8 @@ var initDateRangePicker = function(){
             }
         },
         function(start, end) {
-            daterangeStart = start;
-            daterangeEnd = end;
+            /*daterangeStart = start;
+            daterangeEnd = end;*/
         }
     );
 };
