7343a796966e8a81ca669ea2020fd1b274a51b2d
Striker <mironov1708@gmail.com>
2015-05-31 21:25:35 +0300

    device add on user!

--- server/Arescon/target/data/scripts/chart.js
+++ server/Arescon/target/data/scripts/chart.js
@@ -267,6 +267,28 @@ $('.remove_device').click(function(e){
     });   
     e.preventDefault();
 });
+
+$('#device-add').click(function() {
+    var obj = {};
+    obj.name = $('#device_name').val();
+    obj.serial = $('#device_serial').val();
+    obj.type = $('#device_type').val();
+    obj.impulseID = $('#device_impulse').val();
+    //device_on_datetime
+    //device_off_datetime
+    obj.nextCheck = parseRusDate($('#next_checking_date').val()).getTime();
+    //manual_mode_datetime
+    //manual_mode_value
+    obj.odnFlag = $('#home_common_counter_flag').val();
+    obj.rateFlag = $('#tariff').val();
+    obj.resolution = $('#device_capacity').val();
+    obj.transform = $('#transformation_coefficient').val();
+    obj.periodic = $('#values_registration_way').val();
+
+    $.post('/device/create', obj, function(data){
+
+    });
+});
  
 
 
@@ -835,4 +857,9 @@ var getTimeFormatddmmyyyy = function(date_ms){
     var current_date = new Date(date_ms);
     var date_str = current_date.getDate() + "." + (current_date.getMonth()+1) + "." + current_date.getFullYear();
     return date_str;
+}
+
+var parseRusDate = function(date_str){
+    var dtArr = date_str.split('.');
+    return new Date(dtArr[2], parseInt(dtArr[1])-1, dtArr[0]);
 }
